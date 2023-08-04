9a3939f43971eb5c15482ad76bfc616ded14f6f5
mimanshi-plivo <mimanshi.tiwari@plivo.com>
2023-08-04 15:58:58 +0530

    feat: added top banner

---
+++ src/components/TopBanner/TopBanner.stories.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { TopBanner, BannerType } from './'
+import { Button } from '../Button'
+
+export default {
+  title: 'Components/TopBanner',
+  component: TopBanner,
+  argTypes: {
+    type: {
+      options: BannerType,
+      control: { type: 'select' },
+    },
+  },
+}
+
+const Template = (args) => <TopBanner {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  visible: true,
+  content: 'This is default type banner.',
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  visible: true,
+  content: 'This is success type banner.',
+  type: 'success',
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  visible: true,
+  content: 'This is danger type banner.',
+  type: 'danger',
+}
+
+export const Warning = Template.bind({})
+Warning.args = {
+  visible: true,
+  content: 'This is warning type banner.',
+  type: 'warning',
+}
+
+export const BannerWithButton = Template.bind({})
+BannerWithButton.args = {
+  visible: true,
+  content: 'This is banner with action button.',
+  type: 'default',
+  buttons: [
+    <Button
+      label="Alert"
+      key="button"
+      onClick={() => alert('Banner button clicked!')}
+      size="small"
+      type="link"
+    />,
+  ],
+}
+
+export const BannerWithLoading = Template.bind({})
+BannerWithLoading.args = {
+  visible: true,
+  content: 'This is banner with action button.',
+  type: 'danger',
+  loading: true,
+}
---
+++ src/components/TopBanner/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Block } from '../Block'
+import { Icon } from '../Icon/index'
+import { Text } from '../Typography/index'
+import './topbanner.scss'
+
+export const BannerType = ['warning', 'danger', 'success', 'default']
+export const TopBanner = ({
+  type = 'default',
+  content,
+  buttons,
+  loading,
+  visible,
+  loadingText = 'Loading',
+}) => {
+  return visible ? (
+    <Block
+      className={`top-banner ${type}`}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {loading && (
+        <Block>
+          <Icon.Loading size={16} color="white" />
+        </Block>
+      )}
+      <Text type="headline" color="white">
+        {loading ? loadingText : content}
+      </Text>
+      {!loading && buttons && (
+        <Block display="flex" className="top-banner-buttons-container">
+          {buttons.map((button) => button)}
+        </Block>
+      )}
+    </Block>
+  ) : null
+}
