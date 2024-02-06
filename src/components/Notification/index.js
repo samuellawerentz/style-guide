6fbb36400457cf4b5a426ea05e3462a992322b9c
mimanshi-plivo <mimanshi.tiwari@plivo.com>
2024-02-06 14:23:27 +0530

    cnto-8202 notification added

---
+++ src/components/Notification/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import { Icon } from '../Icon/index'
+import { Text } from '../Typography/index'
+import { notification } from 'antd'
+import './notification.scss'
+
+export const showNotification = (type, message) => {
+  const iconMap = {
+    info: 'info',
+    warning: 'error',
+    error: 'report',
+    success: 'check_circle',
+  }
+
+  notification[type]({
+    message: <Text type="title-3">{message}</Text>,
+    className: `notification-${type}`,
+    icon: <Icon name={iconMap[type]} size={24} />,
+    duration: 5,
+  })
+}
+
+export const Notification = ({ type, message }) => {
+  useEffect(() => {
+    console.log('showNotification', showNotification)
+    showNotification(type, message)
+  }, [type])
+  return <>This is notification of type {type}</>
+}
