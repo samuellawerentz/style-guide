5a8ea6a8e8813e1ae69b512b6901e529559315de
mimanshi-plivo <mimanshi.tiwari@plivo.com>
2024-02-13 12:58:38 +0530

    reverted svg icon changes for succes notification

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -3,27 +3,18 @@ import './styles.scss'
 import PropTypes from 'prop-types'
 import { Icon } from '../Icon/index'
 import { Text } from '../Typography/index'
-import { ReactComponent as CheckCircle } from '../../assets/ic-check-circle.svg'
 
 const iconMap = {
-  name: {
-    info: 'info',
-    warning: 'error',
-    danger: 'report',
-  },
-  svg: {
-    success: CheckCircle,
-  },
-  color: {
-    success: 'success-color',
-  },
+  info: 'info',
+  warning: 'error',
+  danger: 'report',
+  success: 'check_circle',
 }
 
 /**
  * Component that is used to show alert messages on the UI
  */
 export const Alert = ({ className = '', type, message, size = 'default', ...props }) => {
-  const iconColor = iconMap.svg[type] ? iconMap.color[type] : ''
   return (
     <div
       className={[
@@ -35,12 +26,7 @@ export const Alert = ({ className = '', type, message, size = 'default', ...prop
       {...props}
     >
       <div className="contacto-alert-icon">
-        <Icon
-          name={iconMap.name[type] || ''}
-          svg={iconMap.svg[type] || ''}
-          size={20}
-          color={iconColor}
-        />
+        <Icon name={iconMap[type]} size={20} />
       </div>
       <div className="contacto-alert-message">
         <Text>{message}</Text>
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -3,39 +3,22 @@ import { Icon } from '../Icon/index'
 import { Text } from '../Typography/index'
 import { notification } from 'antd'
 import './notification.scss'
-import { ReactComponent as CheckCircle } from '../../assets/ic-check-circle.svg'
 
 const iconMap = {
-  name: {
-    info: 'info',
-    warning: 'error',
-    error: 'report',
-  },
-  svg: {
-    success: CheckCircle,
-  },
-  color: {
-    success: 'success-color',
-  },
+  info: 'info',
+  warning: 'error',
+  error: 'report',
+  success: 'check_circle',
 }
-
 export const showNotification = (type, message) => {
-  const iconColor = iconMap.svg[type] ? iconMap.color[type] : ''
   notification[type]({
     message: <Text type="title-3">{message}</Text>,
     className: `notification-${type}`,
-    icon: (
-      <Icon
-        name={iconMap.name[type] || ''}
-        svg={iconMap.svg[type] || ''}
-        size={24}
-        color={iconColor}
-      />
-    ),
+    icon: <Icon name={iconMap[type]} size={24} />,
     closeIcon: (
       <Icon name="close" size={24} color="primary-text-color" hoverColor="primary-text-color" />
     ),
-    duration: 5,
+    duration: 0,
   })
 }
 
